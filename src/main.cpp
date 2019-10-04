3b2a0aba963f3924fae75c5c826e467f01d27b25
Иванов Кирилл Андреевич <ivanov.k@ez.local>
2019-10-04 14:10:44 +0700

    создал вершинный и фрагментный шейдер, собрал шейдернуя программу. Создал vertex array object и vertex buffer object. В объекте VAO с помощью VBO загрузил массив вершин треугольника в буфер GL_ARRAY_BUFFER

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,17 @@
 
 void key_callback (GLFWwindow*,int,int,int,int);
 
+const GLchar * vertex_shader_source =   "#version 330 core\n"
+                                        "layout (location = 0) in vec3 position;\n" //layout (location = 0) не понятно
+                                        "void main(){\n"
+                                        "   gl_Position = vec4(position, 1.0);\n"
+                                        "}\n\0";
+const GLchar * fragment_sharer_source = "#version 330 core\n"
+                                        "out vec4 color;\n"
+                                        "void main(){\n"
+                                        "   color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                        "}\n\0";
+
 int main(){
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -28,12 +39,66 @@ int main(){
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     glfwSetKeyCallback(window, key_callback);
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertex_shader, sizeof(infoLog), NULL, infoLog);
+        std::cout << "VERTEX SHADER COMPILE FAILED\n" << infoLog << std::endl;
+    }
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_sharer_source, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(fragment_shader, sizeof(infoLog), NULL, infoLog);
+        std::cout << "FRAGMENT SHADER COMPILE FAILED\n" << infoLog << std::endl;
+    }
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+
+    GLfloat vertices [] = {
+        0.0f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
         glClearColor(0.1f, 0.4f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
     glfwTerminate();
     return 0;
 }
