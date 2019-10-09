538100092abd197a9946cedd652b2c7ec8f3e0ad
Иванов Кирилл Андреевич <ivanov.k@ez.local>
2019-10-09 18:16:36 +0700

    начал писать класс mesh

---
+++ src/mesh.cpp
@@ -0,0 +1,23 @@
+#include "mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+{
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	setupMesh();
+}
+
+void Mesh::setupMesh(){
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+
+}
