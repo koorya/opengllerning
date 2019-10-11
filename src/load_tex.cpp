afc169dcdad0ca2caaf529c32966233a366a0ba4
Иванов Кирилл Андреевич <ivanov.k@ez.local>
2019-10-11 19:34:21 +0700

    Текстуры теперь загружаются с альфаканалом. Далее в уроке описано, что при выводе полупрозрачных объектов - нужно вручную сортировать по удаленности, либо использовать еще более хитрые алгоритмы. По простому не делается, да и не особо требуется

--- src/load_tex.cpp
+++ src/load_tex.cpp
@@ -12,13 +12,13 @@
 unsigned int loadTexture(const char * file_path){
 	std::cout<<file_path<<std::endl;
 	int width, height;
-	unsigned char * image = SOIL_load_image(file_path, &width, &height, 0, SOIL_LOAD_RGB);
+	unsigned char * image = SOIL_load_image(file_path, &width, &height, 0, SOIL_LOAD_RGBA);
 	if (image == 0)
 		std::cout<<"LOAD IMAGE FAILED"<<std::endl;
 	GLuint texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
