dfc056d4cba51944420e9c386bca6ff867fbeccf
Иванов Кирилл Андреевич <ivanov.k@ez.local>
2019-10-30 17:44:04 +0700

    немного поправил расположение генерируемых этажей

--- src/ConstructionContainer.cpp
+++ src/ConstructionContainer.cpp
@@ -8,8 +8,9 @@ ConstructionContainer::ConstructionContainer(){
 
 	float stride = 3500.0;;
 	glm::vec3 column_offset = glm::vec3(-5.600e+03, 507.50f, 5.005e+03); 
-	glm::vec3 hor_bond_offset = glm::vec3(0, 127.0f, -555.0); 
-	glm::vec3 tilt_bond_offset = glm::vec3(0, 600.0f, -465.0); 
+	glm::vec3 hor_bond_offset = glm::vec3(0, 127.0f, -540.0); 
+//	glm::vec3 tilt_bond_offset = glm::vec3(0, 600.0f, -425.0); 
+	glm::vec3 tilt_bond_offset = glm::vec3(0, 600.0f, -463.0); 
 
 	std::vector<glm::mat4> column_matrices;
 	std::vector<glm::mat4> hor_bond_matrices;
@@ -41,10 +42,7 @@ ConstructionContainer::ConstructionContainer(){
 					model = glm::translate(model, glm::vec3(x*stride, -(floor)*3000.0, y*stride) - column_offset);
 					model = glm::translate(model, -tilt_bond_offset);
 					model = glm::rotate(model, glm::radians(63.0f), glm::vec3(1.0, 0.0, 0.0));
-					model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0, 0.0, 1.0));
-					// glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(model));
-					// ourShader.setMaterial(Material::green_plastic);
-					// tilted_bond.Draw(ourShader);
+					model = glm::translate(model, glm::vec3(0.0f, 0.0f, 30.0f));
 					tilt_bond_matrices.push_back(model);
 
 					model = glm::scale(glm::mat4(1.0f), glm::vec3(0.002f));
@@ -52,10 +50,7 @@ ConstructionContainer::ConstructionContainer(){
 					model = glm::translate(model, glm::vec3(0.0, 0.0, 2*tilt_bond_offset.z + stride) );
 					model = glm::translate(model, -tilt_bond_offset );
 					model = glm::rotate(model, glm::radians(117.0f), glm::vec3(1.0, 0.0, 0.0));
-					model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0, 0.0, 1.0));
-					// glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(model));
-					// ourShader.setMaterial(Material::green_plastic);
-					// tilted_bond.Draw(ourShader);
+					model = glm::translate(model, glm::vec3(0.0f, 0.0f, 30.0f));
 					tilt_bond_matrices.push_back(model);
 				}
 
@@ -74,11 +69,7 @@ ConstructionContainer::ConstructionContainer(){
 					model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0, 1.0, 0.0));
 					model = glm::translate(model, -tilt_bond_offset);
 					model = glm::rotate(model, glm::radians(63.0f), glm::vec3(1.0, 0.0, 0.0));
-					model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0, 0.0, 1.0));
-
-					// glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(model));
-					// ourShader.setMaterial(Material::green_plastic);
-					// tilted_bond.Draw(ourShader);
+					model = glm::translate(model, glm::vec3(0.0f, 0.0f, 30.0f));
 					tilt_bond_matrices.push_back(model);
 
 					model = glm::scale(glm::mat4(1.0f), glm::vec3(0.002f));
@@ -87,11 +78,7 @@ ConstructionContainer::ConstructionContainer(){
 					model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0, 1.0, 0.0));
 					model = glm::translate(model, -tilt_bond_offset );
 					model = glm::rotate(model, glm::radians(117.0f), glm::vec3(1.0, 0.0, 0.0));
-					model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0, 0.0, 1.0));
-
-					// glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(model));
-					// ourShader.setMaterial(Material::green_plastic);
-					// tilted_bond.Draw(ourShader);
+					model = glm::translate(model, glm::vec3(0.0f, 0.0f, 30.0f));
 					tilt_bond_matrices.push_back(model);
 				}
 			}
