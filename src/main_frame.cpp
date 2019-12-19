4bdfca73a598de74f0ec0997f81153f20e8bb24e
Иванов Кирилл Андреевич <ivanov.k@ez.local>
2019-12-19 16:07:35 +0700

    сдвинул начала рельс на раме

--- src/main_frame.cpp
+++ src/main_frame.cpp
@@ -12,8 +12,8 @@ MainFrame::MainFrame(){
 
 void MainFrame::calcMatrices(){
     this->A = glm::translate(this->World, glm::vec3(0.0, 0.0, this->frame_level));
-	this->rail1 = glm::translate(this->A, glm::vec3(350.0, 6760.0, -400.0));
-	this->rail2 = glm::translate(this->A, glm::vec3(350.0, -245.0, -400.0));
-	this->rail3 = glm::translate(this->A, glm::vec3(350.0, -7250.0, -400.0));
+	this->rail1 = glm::translate(this->A, glm::vec3(3300.0, 6760.0, -400.0));
+	this->rail2 = glm::translate(this->A, glm::vec3(3300.0, -245.0, -400.0));
+	this->rail3 = glm::translate(this->A, glm::vec3(3300.0, -7250.0, -400.0));
 }
 
